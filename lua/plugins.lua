0060c901f694106d2de6819bd2060865e9d1150f
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-11-06 22:04:53 +0900

    add treesitter, lspkind, cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,13 +13,15 @@ packer.startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use 'onsails/lspkind-nvim' -- VSCode like pictograms
+  use 'L3MON4D3/LuaSnip' -- Snippet
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
   use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   use {
-    'windwp/nvim-autopairs',
-    config = function() require("nvim-autopairs").setup {} end
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
+  use { 'windwp/nvim-autopairs' }
   use { 'neoclide/coc.nvim', branch = 'release' }
 end)
