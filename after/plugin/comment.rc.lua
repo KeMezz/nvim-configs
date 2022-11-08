4cf2ab0f6634e1e576cbf243e438dd99fe6dc3de
KeMezz <pnescene@icloud.com>
2022-11-08 21:49:39 +0900

    connect ts-commentstring and nvim-comment

--- after/plugin/comment.rc.lua
+++ after/plugin/comment.rc.lua
@@ -1,4 +1,8 @@
 local status, comment = pcall(require, "nvim_comment")
 if (not status) then return end
 
-comment.setup({})
+comment.setup({
+  hook = function()
+    require("ts_context_commentstring.internal").update_commentstring()
+  end,
+})
